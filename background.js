9a361bf71a8c1247c0da2f8b18fbf133384fe489
Petr Kubát <petr.kubat@gmail.com>
2018-07-21 11:33:40 +0200

    added more info about book comparison. polished tootltips.

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-// TODO create popup with progress
-// TODO invert sequence (currently processing from the bottom it seems)
 // TODO use colours to highlight better matches
 // tODO publish to chrome store
 // TODO write to goodreads discussion
@@ -7,16 +5,20 @@
 var compatibilityUrlBase = "https://www.goodreads.com/book/compatibility_results?id=";
 var compareUrlBase = "https://www.goodreads.com/user/compare/";
 
-var re = /\d{1,3}%/;
-var userCache = {}
+var reComparison = /\d{1,3}/;
+var reCommon = / \d{1,3} /;
+var userCache = new Map()
 
-function setLinks(links, userData) {
+
+
+
+function setLinks(userData) {
     countProcessed++;
     progress.setContent('Processed ' +countProcessed+ ' from ' + countAll)
         .delay(5)
         .push('Processed ' +countProcessed+ ' from ' + countAll);
 
-    links.forEach(link => {
+    userData.links.forEach(link => {
         setLink(link, userData);
     });
 }
@@ -37,11 +39,20 @@ function setLink(link, userData) {
     //link.text(userName + " (Tastes: "+ comparison +", Compatibility: " + compatibility + ")");
     var compareUrl = compareUrlBase + userData.id;
     var compatibilityUrl = compatibilityUrlBase + userData.id;
-    link
-        .after("<a class='goodTooltip' title='test!' href='"+compatibilityUrl+"'> [Compatibility: "+userData.compatibility+"]</a>")
-        .after("<a class='goodTooltip' title='test!' href='"+compareUrl+"'> [Tastes: "+userData.comparison+"] </a>");
+    var comparison = Number(userData.comparison);
+    var style = "style='color:red;'"
+    if(comparison > 50) {
+        style = "style='color:lightgreen;'"
+    }
 
-        tippy(".goodTooltip");
+    var tooltip = userData.comparisonText + "\n" + userData.booksInCommonText;
+
+    link
+        //.after("<a class='goodTooltip' title='test!' href='"+compatibilityUrl+"'> [Compatibility: "+userData.compatibility+"]</a>")
+        .after("<a class='goodTooltip' "+style+" title='"+tooltip+"' href='"+compareUrl+"'> ["+userData.comparison+"% / "+userData.booksInCommon+"] </a>");
+
+        
+    tippy(".goodTooltip");
 
 }
 
@@ -53,7 +64,7 @@ function setLinkError(link, error) {
 function processCompatibilityData(userData, compatibilityData) {
     var div = $("div.leftContainer>div.mediumText>div",compatibilityData);    
     if(div.length !== 0) {
-        var match = div[0].innerText.match(re);
+        var match = div[0].innerText.match(reComparison);
         if(match !== null) {
             var compatibility = match[0];
             userData.compatibility = compatibility;
@@ -71,10 +82,13 @@ function processCompareData(userData, compareData){
     
     var para = $("p.readable", compareData);
     if(para.length !== 0) {
-        var match = para[0].innerText.match(re);
-        if(match !== null) {
-            var comparison = match[0];
+        var matchComparison = para[0].innerText.match(reComparison);
+        if(matchComparison !== null) {
+            var comparison = matchComparison[0];
             userData.comparison = comparison;
+            userData.comparisonText = para[0].innerText;
+            
+            /*
             var compatibilityUrl =  compatibilityUrlBase + userData.id;
             $.ajax({
                 url: compatibilityUrl, 
@@ -82,9 +96,24 @@ function processCompareData(userData, compareData){
             }).done(function(compatibilityData) {
                 processCompatibilityData(this, compatibilityData);
             });
+            */
         } else {
             setLinksError(userData.links, para.text());
         }
+
+        var div = $("div.readable:nth-child(2)", compareData);
+        if(div.length !== 0) {
+
+            var matchCommon = div.text().match(reCommon);
+            if(matchCommon !== null) {
+                var common = matchCommon[0];
+                userData.booksInCommon = common;
+            }
+            userData.booksInCommonText = div.text().trim();
+        }
+
+        setLinks(userData);
+
     } else {
         // probably private
         setLinksError(userData.links, "probably private");
@@ -100,9 +129,17 @@ function cacheThisUser(link) {
         var userData = userCache[userId];
         userData.links.push(link);
     } else {
-        var userData = { id: userId, links: [], comparison: '', compatibility: '' };
+        var userData = { 
+            id: userId, 
+            links: [], 
+            comparison: '', 
+            compatibility: '', 
+            comparisonText: '', 
+            booksInCommon: '',
+            booksInCommonText: '' 
+        };
         userData.links.push(link);
-        userCache[userId] = userData;
+        userCache.set(userId, userData);
     }
 }
 
@@ -131,15 +168,17 @@ $("table.tableList tr td:nth-child(3) a:nth-child(1)").each(function (){
     cacheThisUser(link);
 });
 
-var countAll = Object.keys(userCache).length;
+var countAll = userCache.size;
 var countProcessed = 0;
 
 alertify.set('notifier','position', 'top-right');
 var progress = alertify.message('Spotted '+ countAll +' users on page. Processing starts now. ');
 progress.delay(10);
 
-for(var userId in userCache) {
-    var userData = userCache[userId];
+for (var [key, value] of userCache) {
+//for(var userId in userCache) {
+    var userId = key;
+    var userData = value;
     var compareUrl = compareUrlBase + userId;
     $.ajax({
         url: compareUrl, 
