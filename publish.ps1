ec82d374b3768576d1cd61af5a8f44ee5864babf
Petr Kubát <petr.kubat@gmail.com>
2018-07-24 19:27:36 +0200

    folder structure cleanup, added basic publish script,

---
+++ publish.ps1
@@ -0,0 +1,15 @@
+$version = "v0.3"
+$product = "GoodreadsCompareBooks"
+$repo = "D:\_sources\GoodreadsCompareBooks"
+$releaseDest = "{0}\{1}" -f $repo,".releases"
+$releaseFullPath = "{0}\{1}.{2}.{3}" -f $releaseDest,$product,$version,"zip"
+
+$excludes = @(
+    ".git",
+    ".github",
+    ".releases",
+    "screenshots",
+    ".gitignore"
+)
+$filesToPack = Get-ChildItem -Path $repo -Exclude $excludes
+Compress-Archive -Path $filesToPack -DestinationPath $releaseFullPath
